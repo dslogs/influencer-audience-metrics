9306685899972ae95b4d457ddd9216d9d451c7c6
dslogs <derik@slogansocial.com>
2026-01-02 10:47:37 -0800

    Make the function return a typed dict for getting attachments etc

--- at.py
+++ at.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from pyairtable import Api
 import requests
+from typing import TypedDict
 
 load_dotenv()
 
@@ -9,20 +10,25 @@ AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
 AIRTABLE_BASE_ID = os.getenv("AIRTABLE_BASE_ID")
 api = Api(AIRTABLE_API_KEY)
 
-if __name__ == "__main__":
 
+class InfluencerMetrics(TypedDict):
+    tt_location: bytes | None
+
+
+def get_influencer_metric_attachments(record_id: str) -> InfluencerMetrics:
     audience_metrics = api.table(AIRTABLE_BASE_ID, "tbleVAs7oNLDhUwAk")
-    records = audience_metrics.all(use_field_ids=True)
-    print(records)
 
-    for record in records:
-        fields = record.get('fields')
-        tt_location = 'fldSSYdaauiP4dKGk'
-        attachment_array = fields.get(tt_location)
-        if attachment_array and len(attachment_array) > 0:
-            first_pic = attachment_array[0]
-            response = requests.get(first_pic['url'])
-            image_bytes = response.content
+    record = audience_metrics.get(record_id, use_field_ids=True)
+    fields = record.get('fields')
+    tt_location_field_id = 'fldSSYdaauiP4dKGk'
+    attachment_array = fields.get(tt_location_field_id)
+
+    tt_location_bytes = None
+    if attachment_array and len(attachment_array) > 0:
+        first_pic = attachment_array[0]
+        response = requests.get(first_pic['url'])
+        tt_location_bytes = response.content
+
+    return InfluencerMetrics(tt_location=tt_location_bytes)
 
 
-    # print(records)
